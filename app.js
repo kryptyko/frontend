b63b972bb02451a7bfb86cdd0feb19d095555874
krypto <kryptyko@gmail.com>
2023-09-23 13:41:42 -0300

    agregados los mensajes de chat

--- app.js
+++ app.js
@@ -1,39 +1,65 @@
 // Obtener referencias a los elementos relevantes del DOM
 const serverList = document.querySelector('.server-list');
 const roomList = document.querySelector('.room-list');
+const messageContainer = document.querySelector('.message-container');
 
 // Agregar un event listener a la lista de servidores
 serverList.addEventListener('click', function(event) {
   // Verificar si se hizo clic en un elemento de la lista de servidores
   if (event.target.tagName === 'LI') {
-    // obtengo el srv seleccionado
+    // Obtener el servidor seleccionado
     const selectedServer = event.target.textContent;
     
-    // aca deberia llamar a la base en busca de los servidores en la bd
+    // Simular una función para obtener los canales del servidor seleccionado
     const channels = getChannels(selectedServer);
     
-    // limpia la lsita de salas (pt..o salas que no enseña nada)
+    // Limpiar la lista de salas existente
     roomList.innerHTML = '';
     
-    // agrego los canales a la lista de salas
+    // Agregar los canales del servidor seleccionado a la lista de salas
     channels.forEach(function(channel) {
       const li = document.createElement('li');
       li.textContent = channel;
       roomList.appendChild(li);
     });
     
-    // marco un srv como activo (borro el tag anterior y sobreescribo uno nuevo)
+    // Marcar el servidor seleccionado como activo
     const activeServer = serverList.querySelector('.active');
     if (activeServer) {
       activeServer.classList.remove('active');
     }
     event.target.classList.add('active');
+    
+    // Limpiar el contenedor de mensajes
+    messageContainer.innerHTML = '';
   }
 });
 
-// funcion que llama a la lista de servidores(por ahora devuelve algo sin bd)
+// Agregar un event listener a la lista de salas
+roomList.addEventListener('click', function(event) {
+  // Verificar si se hizo clic en un elemento de la lista de salas
+  if (event.target.tagName === 'LI') {
+    // Obtener el canal seleccionado
+    const selectedChannel = event.target.textContent;
+    
+    // Simular una función para obtener los mensajes del canal seleccionado
+    const messages = getMessages(selectedChannel);
+    
+    // Limpiar el contenedor de mensajes
+    messageContainer.innerHTML = '';
+    
+    // mostrar los mensajes del canal seleccionado
+    messages.forEach(function(message) {
+      const p = document.createElement('p');
+      p.textContent = message;
+      messageContainer.appendChild(p);
+    });
+  }
+});
+
+// Función simulada para obtener los canales de un servidor
 function getChannels(server) {
-  // esto deberia obtener en un json
+  // Mapa de nombres de canales por servidor
   const channelMap = {
     'srv1': ['General', 'Noticias', 'Eventos'],
     'srv2': ['Discusión', 'Proyectos', 'Soporte'],
@@ -44,6 +70,32 @@ function getChannels(server) {
   // Verificar si el servidor tiene canales definidos en el mapa
   if (server in channelMap) {
     return channelMap[server];
+  } else {
+    return [];
+  }
+}
+
+// Función simulada para obtener los mensajes de un canal
+function getMessages(channel) {
+  // Mapa de mensajes por canal
+  const messageMap = {
+    'General': ['Mensaje 1', 'Mensaje 2', 'Mensaje 3'],
+    'Noticias': ['Noticia 1', 'Noticia 2', 'Noticia 3'],
+    'Eventos': ['Evento 1', 'Evento 2', 'Evento 3'],
+    'Discusión': ['Mensaje A', 'Mensaje B', 'Mensaje C'],
+    'Proyectos': ['Proyecto X', 'Proyecto Y', 'Proyecto Z'],
+    'Soporte': ['Consulta 1', 'Consulta 2', 'Consulta 3'],
+    'Ofertas': ['Oferta A', 'Oferta B', 'Oferta C'],
+    'Ventas': ['Venta X', 'Venta Y', 'Venta Z'],
+    'Compras': ['Compra 1', 'Compra 2', 'Compra 3'],
+    'Juegos': ['Juego 1', 'Juego 2', 'Juego 3'],
+    'Música': ['Canción A', 'Canción B', 'Canción C'],
+    'Deportes': ['Deporte X', 'Deporte Y', 'Deporte Z']
+  };
+  
+  // Verificar si el canal tiene mensajes definidos en el mapa
+  if (channel in messageMap) {
+    return messageMap[channel];
   } else {
     return [];
   }
